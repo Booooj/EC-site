8b2404a54db44ab396144be199e813d9e6d442d6
Booooj <90837337+Booooj@users.noreply.github.com>
2022-05-25 13:16:52 +0900

    Update staff_add.php

--- staff_add.php
+++ staff_add.php
@@ -4,25 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>六丸農園</title>
+    <title></title>
 </head>
 <body>
-    スタッフ追加<br/>
-    <br />
-    <form method="post" action="staff_add_check.php">
-        スタッフ名を入力してください。<br />
-        <input type="text" name="name" style="width:200px"><br />
-        パスワードを入力してください。<br />
-        <input type="password" name="pass" sytle="width:100px"><br />
-        パスワードをもう一度入力してください。
-        <input type="password" name="pass" sytle="width:100px"><br />
-        <br />
-        <input type="button" onclick="history.back()"value="戻る">
-        <input type="submit"value="OK">
-</form>
-    <?php
- 
-   ?>
-
+   
 </body>
 </html>
