6acba009280edeabdfba364adc2e5cdaf55bc4a5
Booooj <90837337+Booooj@users.noreply.github.com>
2022-07-18 09:33:31 +0900

    Update add.php

--- add.php
+++ add.php
@@ -4,9 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>スタッフ追加</title>
 </head>
 <body>
-   
+    スタッフ追加<br/>
+    <br />
+    <form method="post" action="staffcheck.php">
+        スタッフ名を入力してください。<br />
+        <input type="text" name="name"><br />
+        パスワードを入力してください。<br />
+        <input type="password" name="password"><br />
+        パスワードをもう一度入力してください。
+        <input type="password" name="password"><br />
+        <br />
+        <input type="button" onclick="back()"value="戻る">
+        <input type="submit"value="保存">
+</form>
+    <?php
+ 
+   ?>
+
 </body>
 </html>
